1855576ed1a1585aa29f3b49b529b0229fc97cd1
Joel Gray <joeltgray@gmail.com>
2025-07-28 15:17:17 +0100

    Bump version

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -16,7 +16,7 @@
  * - components/: Flarum component extensions (PostStream, Post, ReplyComposer)
  * 
  * @author Threadify Extension
- * @version 1.0.2
+ * @version 1.0.3
  */
 
 // Import utility modules
@@ -87,7 +87,7 @@ export { getThreadedPostsCache, isThreadingActive } from './components/ThreadedP
  */
 export function getThreadifyStatus() {
   return {
-    version: '1.0.2',
+    version: '1.0.3',
     name: 'Threadify',
     author: 'syntaxoutlaw',
     isActive: true,
