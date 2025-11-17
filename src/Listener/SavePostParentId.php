1b93b966c6f7715280cdad5974d542daf16a6f6c
Lady-Byron <samantha-everett@outlook.com>
2025-11-17 17:27:42 -0500

    Update SavePostParentId.php

--- src/Listener/SavePostParentId.php
+++ src/Listener/SavePostParentId.php
@@ -6,25 +6,25 @@ use Flarum\Post\Event\Saving;
 
 class SavePostParentId
 {
-    public function handle(Saving $event)
-    {
-        // [!! OPTIMIZED !!] Removed all error_log calls
-        
-        $post = $event->post;
-        $data = $event->data;
-        
-        $parentId = null;
-        
-        if (isset($data['attributes']['parent_id'])) {
-            $parentId = $data['attributes']['parent_id'];
-        } elseif (isset($data['parent_id'])) {
-            $parentId = $data['parent_id'];
-        } elseif (isset($data['relationships']['parent']['data']['id'])) {
-            $parentId = $data['relationships']['parent']['data']['id'];
-        }
-        
-        if ($parentId && is_numeric($parentId) && $parentId > 0) {
-            $post->parent_id = (int) $parentId;
-        }
-    }
+    public function handle(Saving $event)
+    {
+        // [!! OPTIMIZED !!] Removed all error_log calls
+
+        $post = $event->post;
+        $data = $event->data;
+
+        $parentId = null;
+
+        if (isset($data['attributes']['parent_id'])) {
+            $parentId = $data['attributes']['parent_id'];
+        } elseif (isset($data['parent_id'])) {
+            $parentId = $data['parent_id'];
+        } elseif (isset($data['relationships']['parent']['data']['id'])) {
+            $parentId = $data['relationships']['parent']['data']['id'];
+        }
+
+        if ($parentId && is_numeric($parentId) && $parentId > 0) {
+            $post->parent_id = (int) $parentId;
+        }
+    }
 }
